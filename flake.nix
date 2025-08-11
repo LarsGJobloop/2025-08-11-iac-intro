efc68b92fd4029f00cf373253a987620ae071ef3
zabronax <zabronax@gmail.com>
2025-08-11 09:40:58 +0200

    clean up devshell

--- flake.nix
+++ flake.nix
@@ -9,26 +9,28 @@
     { self, nixpkgs }:
     let
       withSystem = nixpkgs.lib.genAttrs [
-        "x86_64-linux" # TODO! NOT Verified
+        "x86_64-linux" # Verified
         "x86_64-darwin" # TODO! NOT Verified
         "aarch64-linux" # TODO! NOT Verified
         "aarch64-darwin" # Verified
       ];
-      withPkgs = callback: withSystem (system: callback (import nixpkgs { inherit system; }));
+      withPkgs =
+        callback:
+        withSystem (
+          system:
+          callback (
+            import nixpkgs {
+              inherit system;
+              config.allowUnfree = true;
+            }
+          )
+        );
     in
     {
       devShells = withPkgs (pkgs: {
         default = pkgs.mkShell {
           packages = with pkgs; [
-            # Application
-            nodejs_22
-            pnpm
-
-            # Project Management
-            opentofu # Infrastructure and SaaS management
-            just # Task runner
-            sops # Secrets managements
-            age # Encryption tool
+            terraform # Infrastructure as Code
           ];
         };
       });
