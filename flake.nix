fef682df6edaca2909050cda54372476484a6737
zabronax <zabronax@gmail.com>
2025-08-11 09:38:46 +0200

    setup development environment

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "An backend project covering several advanced concepts";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      withSystem = nixpkgs.lib.genAttrs [
+        "x86_64-linux" # TODO! NOT Verified
+        "x86_64-darwin" # TODO! NOT Verified
+        "aarch64-linux" # TODO! NOT Verified
+        "aarch64-darwin" # Verified
+      ];
+      withPkgs = callback: withSystem (system: callback (import nixpkgs { inherit system; }));
+    in
+    {
+      devShells = withPkgs (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            # Application
+            nodejs_22
+            pnpm
+
+            # Project Management
+            opentofu # Infrastructure and SaaS management
+            just # Task runner
+            sops # Secrets managements
+            age # Encryption tool
+          ];
+        };
+      });
+
+      formatter = withPkgs (pkgs: pkgs.nixfmt-rfc-style);
+    };
+}
